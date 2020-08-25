38ea500008abff5bfb9abcfc38cb43e26fd1ef75
tanveershuvo <tanveershuvos@gmail.com>
2020-08-25 23:00:38 +0600

    All ok

--- add_edit_season.php
+++ add_edit_season.php
@@ -2,10 +2,11 @@
 <?php include_once 'dbCon.php'; ?>
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="UTF-8">
 	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-	<title><?php echo $siteName;?></title>
+	<title><?php echo $siteName; ?></title>
 
 	<!-- Google Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
@@ -21,17 +22,17 @@
 	<!-- Custom Css -->
 	<link href="css/style.css" rel="stylesheet">
 	<style media="screen">
-	.card {
-		background: #272643;
-		min-height: 30px;
-		box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
-		position: relative;
-		margin-bottom: 30px;
-		-webkit-border-radius: 2px;
-		-moz-border-radius: 2px;
-		-ms-border-radius: 2px;
-		border-radius: 2px;
-	}
+		.card {
+			background: #272643;
+			min-height: 30px;
+			box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+			position: relative;
+			margin-bottom: 30px;
+			-webkit-border-radius: 2px;
+			-moz-border-radius: 2px;
+			-ms-border-radius: 2px;
+			border-radius: 2px;
+		}
 	</style>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
@@ -43,90 +44,84 @@
 	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 	<?php
 	include_once 'dbCon.php';
-	$conn= connect();
-	$sql= "SELECT MAX(sea_end_time) as mx from season";
+	$conn = connect();
+	$comID = $_SESSION['com_id'];
+	$sql = "SELECT MAX(sea_end_time) as mx from season WHERE com_id = $comID";
 
 	$result = $conn->query($sql);
-	foreach($result as $row){
+	foreach ($result as $row) {
 		$id = $row['mx'];
 	}
 	?>
 	<script>
-	$( function() {
-		var dateFormat = "mm/dd/yy",
-		from = $( "#from" )
-		.datepicker({
-			defaultDate: "+1w",
-			changeMonth: true,
-			numberOfMonths: 1,
-			minDate: new Date("<?=$id?>"),
-		})
-		.on( "change", function() {
-			to.datepicker( "option", "minDate", getDate( this ) );
-		}),
-		to = $( "#to" ).datepicker({
-			defaultDate: "+1w",
-			changeMonth: true,
-			numberOfMonths: 1,
-		})
-		.on( "change", function() {
-			from.datepicker( "option", "maxDate", getDate( this ) );
-		});
+		$(function() {
+			var dateFormat = "mm/dd/yy",
+				from = $("#from")
+				.datepicker({
+					defaultDate: "+1w",
+					changeMonth: true,
+					numberOfMonths: 1,
+					minDate: new Date("<?= $id ?>"),
+				})
+				.on("change", function() {
+					to.datepicker("option", "minDate", getDate(this));
+				}),
+				to = $("#to").datepicker({
+					defaultDate: "+1w",
+					changeMonth: true,
+					numberOfMonths: 1,
+				})
+				.on("change", function() {
+					from.datepicker("option", "maxDate", getDate(this));
+				});
 
-		function getDate( element ) {
-			var date;
-			try {
-				date = $.datepicker.parseDate( dateFormat, element.value );
-			} catch( error ) {
-				date = null;
+			function getDate(element) {
+				var date;
+				try {
+					date = $.datepicker.parseDate(dateFormat, element.value);
+				} catch (error) {
+					date = null;
+				}
+
+				return date;
 			}
-
-			return date;
-		}
-	} );
+		});
 	</script>
 
 </head>
 <!-- Large Size -->
 <?php
 include_once 'dbCon.php';
-$conn= connect();
-if (isset($_POST['add'])){
+$conn = connect();
+if (isset($_POST['add'])) {
 	$name = $_POST['name'];
 	$start = $_POST['from'];
 	$end = $_POST['to'];
 	$budget = $_POST['budget'];
-	$comID = $_SESSION['com_id'];
-	$sql= "SELECT MAX(sea_end_time) as mx from season";
+	$sql = "SELECT MAX(sea_end_time) as mx from season WHERE com_id = $comID";
 
 	$result = $conn->query($sql);
-	if($result->num_rows > 0){
-		foreach($result as $row){
+	if ($result->num_rows > 0) {
+		foreach ($result as $row) {
 			$id = $row['mx'];
 			$dr = date('m-d-Y');
-			if ($id < $dr){
-				$sql= "INSERT INTO season (sea_name,com_id,sea_start_time,sea_end_time,sea_budget) VALUES ('$name','$comID','$start','$end','$budget')";
+			if ($id < $dr) {
+				$sql = "INSERT INTO season (sea_name,com_id,sea_start_time,sea_end_time,sea_budget) VALUES ('$name','$comID','$start','$end','$budget')";
 
 				$conn->query($sql);
 				echo '<script>window.location.href = "season_details"</script>';
-			}
-
-			else
-			{
-				echo '<script>alert("Current season ends at '."$id".' . Select following date to create new season");</script>';
+			} else {
+				echo '<script>alert("Current season ends at ' . "$id" . ' . Select following date to create new season");</script>';
 			}
 		}
-
 	}
-
-
 }
-if (isset($_GET['id'])){
+if (isset($_GET['id'])) {
 
 	$id = $_GET['id'];
 	$sql = "Select * from season where sea_id='$id'";
 	$result = $conn->query($sql);
-	foreach($result as $row){
+	foreach ($result as $row) {
 		$name = $row['sea_name'];
 		$start = $row['sea_start_time'];
 		$end = $row['sea_end_time'];
@@ -134,23 +129,24 @@ if (isset($_GET['id'])){
 	}
 }
 
-if (isset($_POST['edit'])){
+if (isset($_POST['edit'])) {
 	$sid = $_GET['id'];
 	$name = $_POST['name'];
 	$start = $_POST['from'];
 	$end = $_POST['to'];
 	$budget = $_POST['budget'];
 	$comID = $_SESSION['com_id'];
-	$sql= "Update season SET sea_name='$name',com_id='$comID',sea_start_time='$start',sea_end_time='$end',sea_budget='$budget' Where sea_id='$sid' ";
+	$sql = "Update season SET sea_name='$name',com_id='$comID',sea_start_time='$start',sea_end_time='$end',sea_budget='$budget' Where sea_id='$sid' ";
 	//echo $sql;exit;
 	$conn->query($sql);
 	echo '<script>window.location.href = "season_details"</script>';
 }
 ?>
+
 <body class="login-page">
 	<div class="msg">
 		<h4>
-			<?php if (!isset($_GET['id'])){ ?>
+			<?php if (!isset($_GET['id'])) { ?>
 				Add / Save
 			<?php } else { ?>
 				Edit / Update
@@ -158,21 +154,24 @@ if (isset($_POST['edit'])){
 			Season Details
 		</H4>
 
-	</div><hr>
+	</div>
+	<hr>
 	<div class="login-box">
 		<div class="card">
-			<div class="body" >
+			<div class="body">
 
-				<form  method="POST" >
+				<form method="POST">
 					<div class="row clearfix">
 
-						<div class="col-lg-6 col-md-4 col-sm-8 col-xs-8 " >
+						<div class="col-lg-6 col-md-4 col-sm-8 col-xs-8 ">
 							<label for="password_2">Name:</label>
 						</div>
 						<div class="col-lg-6 col-md-8 ">
 							<div class="form-group">
 								<div class="form-line">
-									<input type="text" name="name"  id="name" class="form-control" value="<?php if (isset($name)){ echo $name; }?>" placeholder="Enter Season Name" >
+									<input type="text" name="name" id="name" class="form-control" value="<?php if (isset($name)) {
+																												echo $name;
+																											} ?>" placeholder="Enter Season Name">
 								</div>
 							</div>
 						</div>
@@ -184,14 +183,18 @@ if (isset($_POST['edit'])){
 						<div class="col-lg-3 col-md-10 col-sm-8 col-xs-7">
 							<div class="form-group">
 								<div class="form-line">
-									<input type="text" name="from"  id="from" value="<?php if (isset($start)){ echo $start; }?>" class="form-control"  placeholder="From">
+									<input type="text" name="from" id="from" value="<?php if (isset($start)) {
+																						echo $start;
+																					} ?>" class="form-control" placeholder="From">
 								</div>
 							</div>
 						</div>
 						<div class="col-lg-3 col-md-10 col-sm-8 col-xs-7">
 							<div class="form-group">
 								<div class="form-line">
-									<input type="text" name="to"  id="to" value="<?php if (isset($end)){ echo $end; }?>" class="form-control"  placeholder="To">
+									<input type="text" name="to" id="to" value="<?php if (isset($end)) {
+																					echo $end;
+																				} ?>" class="form-control" placeholder="To">
 								</div>
 							</div>
 						</div>
@@ -204,16 +207,18 @@ if (isset($_POST['edit'])){
 						<div class="col-lg-6 col-md-10 col-sm-8 col-xs-7">
 							<div class="form-group">
 								<div class="form-line">
-									<input type="text" name="budget"  id="pro_name" class="form-control" value="<?php if (isset($budget)){ echo $budget; }?>" placeholder="Enter Season Budget" >
+									<input type="text" name="budget" id="pro_name" class="form-control" value="<?php if (isset($budget)) {
+																													echo $budget;
+																												} ?>" placeholder="Enter Season Budget">
 								</div>
 							</div>
 						</div>
 					</div>
 
-					<?php if (!isset($_GET['id'])){ ?>
-						<button class="btn btn-block btn-lg  waves-effect"   type="submit" name="add"><span class="material-icons">save</span></button>
+					<?php if (!isset($_GET['id'])) { ?>
+						<button class="btn btn-block btn-lg  waves-effect" type="submit" name="add"><span class="material-icons">save</span></button>
 					<?php } else { ?>
-						<button class="btn btn-block btn-lg waves-effect"   type="submit" name="edit"><span class="material-icons">done_outline</span> </button>
+						<button class="btn btn-block btn-lg waves-effect" type="submit" name="edit"><span class="material-icons">done_outline</span> </button>
 					<?php } ?>
 				</form>
 			</div>
@@ -231,4 +236,5 @@ if (isset($_POST['edit'])){
 	<script src="js/admin.js"></script>
 	<script src="js/pages/examples/sign-in.js"></script>
 </body>
+
 </html>
